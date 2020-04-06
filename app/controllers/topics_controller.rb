047b4a035e5bbc8b6ac1c3f7b1a6ab3182151143
hiro <hiro.chibita@gmail.com>
2020-04-06 12:24:25 +0000

    投稿一覧

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,25 @@
 class TopicsController < ApplicationController
+  
+  def index
+    @topics = Topic.all
+  end  
+  
   def new
    @topics = Topic.new
   end 
+  
+  def create
+    #binding.pry
+    @topics = Topic.new(topic_params)
+    if @topics.save
+      redirect_to topic_path, success: '投稿しました'
+    else
+      render :new
+    end
+  end
+  
+  private 
+   def topic_params
+    params.require(:topic).permit(:user_id, :name, :images, :description, :parts, :created_at )
+   end 
 end
