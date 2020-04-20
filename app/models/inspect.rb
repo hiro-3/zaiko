45c74ed50930c21caf955ad76751046c6e2789fe
hiro <hiro.chibita@gmail.com>
2020-04-20 20:14:32 +0000

    有効化

--- app/models/inspect.rb
+++ app/models/inspect.rb
@@ -1,5 +1,17 @@
 class Inspect < ApplicationRecord
   mount_uploader :images,ImageUploader
+  
+  validates :car_name, presence: true
+  validates :name, presence: true
+  validates :process, presence: true
+  validates :worker, presence: true
+  validates :shop_name, presence: true
+  validates :accrual_date, presence: true
+  validates :judge1, presence: true
+  validates :judge2, presence: true
+  validates :judge3, presence: true
+  validates :judge4, presence: true
+  validates :check, presence: true
   validates :mold_type, acceptance: true
   
   belongs_to :user
